b5d8fd78d8d35d86f4f38dde7a4ac05e2a1768a5
Locusclaer <zyh9097@stu.ouc.edu.cn>
2025-12-19 11:01:33 +0800

    feat(RDT_2.2):允许 ACK 数据包出错 - TCP_Receiver：发送上一个包的确认来表达否定

--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -14,7 +14,7 @@ import com.ouc.tcp.tool.TCP_TOOL;
 public class TCP_Receiver extends TCP_Receiver_ADT {
 	
 	private TCP_PACKET ackPack;	//回复的ACK报文段
-	int sequence=1;//用于记录当前待接收的包序号，注意包序号不完全是
+	int sequence=0;//用于记录当前待接收的包序号，注意包序号不完全是
 		
 	/*构造函数*/
 	public TCP_Receiver() {
@@ -26,6 +26,8 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	//接收到数据报：检查校验和，设置回复的ACK报文段
 	public void rdt_recv(TCP_PACKET recvPack) {
 		//检查校验码，生成ACK
+		int dataIndex = (recvPack.getTcpH().getTh_seq() - 1) / recvPack.getTcpS().getData().length;
+
 		if(CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
 			//生成ACK报文段（设置确认号）
 			tcpH.setTh_ack(recvPack.getTcpH().getTh_seq());
@@ -35,13 +37,15 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 			reply(ackPack);			
 			
 			//将接收到的正确有序的数据插入data队列，准备交付
-			dataQueue.add(recvPack.getTcpS().getData());				
-			sequence++;
+			if (dataIndex == sequence) {
+				dataQueue.add(recvPack.getTcpS().getData());
+				sequence++;
+			}
 		}else{
 			System.out.println("Recieve Computed: "+CheckSum.computeChkSum(recvPack));
 			System.out.println("Recieved Packet"+recvPack.getTcpH().getTh_sum());
 			System.out.println("Problem: Packet Number: "+recvPack.getTcpH().getTh_seq()+" + InnerSeq:  "+sequence);
-			tcpH.setTh_ack(-1);
+			tcpH.setTh_ack((sequence - 1) * recvPack.getTcpS().getData().length - 1);
 			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
 			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
 			//回复ACK报文段
