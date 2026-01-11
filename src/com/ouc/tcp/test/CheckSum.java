ad189011928717cfbd6618b12af1012c2ed1f11a
Locusclaer <zyh9097@stu.ouc.edu.cn>
2026-01-11 12:29:38 +0800

    fix(RDT 2.2): - 代码格式修正

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -1,13 +1,12 @@
 package com.ouc.tcp.test;
 
-import java.util.zip.CRC32;
-
 import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 import com.ouc.tcp.message.TCP_SEGMENT;
 
+import java.util.zip.CRC32;
+
 public class CheckSum {
-	
 	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack，以及TCP数据字段*/
 	public static short computeChkSum(TCP_PACKET tcpPack) {
         CRC32 crc32 = new CRC32();
@@ -30,5 +29,4 @@ public class CheckSum {
 
         return (short) crc32.getValue();
 	}
-	
 }
